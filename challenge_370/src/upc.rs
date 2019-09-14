4ab6a45e1153b6d0f1f629f6bfe866d1fd8723de
dashaw92 <dashaw92@gmail.com>
2019-09-14 18:34:09 -0400

    Don't bother padding to 11 digits

--- challenge_370/src/upc.rs
+++ challenge_370/src/upc.rs
@@ -1,6 +1,5 @@
 // Given an input string, "123456", converts it to a Vec<u32> whose elements are the parsed chars: [1,2,3,4,5,6]
 // Will skip invalid base 10 digits: "123ab456" -> [1,2,3,4,5,6]
-// The output vec will be left padded with 0s to be exactly 11 long: "123" -> [0,0,0,0,0,0,0,0,1,2,3]
 // If the input string is > 11, it will be right truncated: "1234567890123" -> [1,2,3,4,5,6,7,8,9,0,1]
 fn to_upc(input: &'_ str) -> Vec<u32> {
     let mut digits: Vec<_> = input.chars().filter_map(|c| char::to_digit(c, 10)).collect();
@@ -12,7 +11,15 @@ fn to_upc(input: &'_ str) -> Vec<u32> {
     }
 
     //pad the vec to be 11 digits long. A upc is 12 digits
-    while digits.len() < 11 {
+    // while digits.len() < 11 {
+    //     digits.insert(0, 0);
+    // }
+
+    //I realized that the padding above is kinda pointless?
+    //It doesn't matter if the vec is 11 digits or n, as long
+    //as it's odd. Padding it to 11 long with 0s is pointless:
+    //0 + n = n
+    if digits.len() < 11 && digits.len() & 1 != 1 {
         digits.insert(0, 0);
     }
 
